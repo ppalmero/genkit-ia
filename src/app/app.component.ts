ffec40f0bd3774fba642367006a224cdf3c40b1e
ppalmero <pablorafaelpalmero@gmail.com>
2025-05-09 17:03:28 -0300

    servidoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { genkit } from 'genkit';
+import { googleAI, gemini20Flash } from '@genkit-ai/googleai';
+import { streamFlow } from 'genkit/beta/client';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+const url = 'http://localhost:8000/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'genkit-ia';
+  count: string = '3';
+  characters: any = undefined;
+  error?: string = undefined;
+  loading: boolean = false;
+  
+  constructor(private http: HttpClient/*, private authService: AutenticacionService*/) { }
+
+  ngOnInit(): void {
+    //this.callFlow();
+
+    this.getServer().subscribe(dataClientes => {
+      console.log("lista clientes"  + dataClientes);
+    })
+
+  }
+
+  getServer(): Observable<string>{
+    return this.http.get<string>(url);
+  }
+  
+  async callFlow() {
+    try {
+      const response = streamFlow({url, input: parseInt(this.count),});
+      for await (const chunk of response.stream) {
+        this.characters = (chunk);
+      }
+      this.loading = (false);
+    } catch (e) {
+      this.loading = false;
+      if ((e as any).cause) {
+        this.error = `${(e as any).cause}`;
+      } else {
+        this.error = `${e}`;
+      }
+    }
+  }
 }
