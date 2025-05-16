b91783a03aaf84f2899cebc72f837313e2103c1e
ppalmero <pablorafaelpalmero@gmail.com>
2025-05-16 14:37:35 -0300

    sinapiapp-config-ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"angular-4e797","appId":"1:845735969236:web:98d133ea8776217d271641","databaseURL":"https://angular-4e797-default-rtdb.firebaseio.com","storageBucket":"angular-4e797.firebasestorage.app","apiKey":"AIzaSyBsyAl4KhKka8IfVmGHfsTrLdRKzvdhDTU","authDomain":"angular-4e797.firebaseapp.com","messagingSenderId":"845735969236"})), provideFirestore(() => getFirestore())]
+  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"angular-4e797","appId":"1:845735969236:web:98d133ea8776217d271641","databaseURL":"https://angular-4e797-default-rtdb.firebaseio.com","storageBucket":"angular-4e797.firebasestorage.app","apiKey":"k","authDomain":"angular-4e797.firebaseapp.com","messagingSenderId":"845735969236"})), provideFirestore(() => getFirestore())]
 };
